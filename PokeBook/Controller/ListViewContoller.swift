de72e5b9cde6755fd85b8be751caf706863f7c24
jbnong07 <jbnong07@naver.com>
2024-12-12 07:00:14 +0900

    ✨ [Feat] ListViewController

--- PokeBook/Controller/ListViewContoller.swift
+++ PokeBook/Controller/ListViewContoller.swift
@@ -7,17 +7,23 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
-    let contactList: ContactList = ContactList()
-
+final class ListViewController: UIViewController {
+    private let contactList: ContactList = ContactList()
+    private let coreDataStack: CoreDataStack = CoreDataStack.shared
+    private var contacts: [ContactData] = []//core data에서 가져올 연락처 데이터
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupContactList()
+        setupLayout()
+        setupNavigationTitle()
+        loadContacts()
     }
     
-    private func setupContactList() {
+    // MARK: setupLayout
+    private func setupLayout() {
         view.addSubview(contactList)
         contactList.translatesAutoresizingMaskIntoConstraints = false
+        contactList.delegate = self
         
         NSLayoutConstraint.activate([
             contactList.topAnchor.constraint(equalTo: view.topAnchor),
@@ -26,5 +32,42 @@ class ListViewController: UIViewController {
             contactList.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
     }
+    
+    // MARK: setupNavigation
+    private func setupNavigationTitle() {
+        navigationItem.title = "Poke Bool"
+        navigationItem.rightBarButtonItem = setNewContactButton()
+    }
+    
+    private func setNewContactButton() -> UIBarButtonItem {
+        let addButton: UIButton = {
+            let button = UIButton(type: .system)
+            button.setTitle("Add", for: .normal)
+            button.addAction(UIAction { [weak self] _ in
+                guard let self = self else { return }
+                self.moveToDetailView()
+            }, for: .touchUpInside)
+            
+            return button
+        }()
+        
+        let barButton = UIBarButtonItem(customView: addButton)
+        return barButton
+    }
+    
+    private func moveToDetailView() {
+        let detailVC = DetailViewController()
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+    
+    // MARK: loadContacts
+    private func loadContacts() {
+           contacts = coreDataStack.fetchContact() // Core Data에서 연락처 데이터 로드
+           contactList.reloadData()
+       }
+}
+
+extension ListViewController: UITableViewDelegate {
+    
 }
 
