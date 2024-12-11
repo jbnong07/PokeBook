f13aed73741271ae2ef8477b29bc95f24ed8a7c5
jbnong07 <jbnong07@naver.com>
2024-12-12 05:55:23 +0900

    ✨ [Feat] detail view

--- PokeBook/Controller/DetailViewContoller.swift
+++ PokeBook/Controller/DetailViewContoller.swift
@@ -6,11 +6,22 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailViewController: UIViewController {
+    private let detailView: DetailView = DetailView()
+    private let networkManager: NetworkManager = NetworkManager()
+    private let coreDataStack: CoreDataStack = CoreDataStack.shared // 싱글톤 객체에 접근
+    
+    var contactID: NSManagedObjectID? //데이터 저장 시 설정하여 이후 로드나 수정 등에 활용할 수 있음. 아직 저장되지 않았거나 초기화 되지 않은 객체의 경우를 고려하여 옵셔널 처리
+    
+    override func loadView() {
+        view = detailView//루트뷰를 detailView로 설정하기 위해 시점을 loadView로 설정함
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
 }
--- PokeBook/Model/URLSession/PokemonData.swift
+++ PokeBook/Model/URLSession/PokemonData.swift
@@ -16,5 +16,5 @@ struct Sprites: Codable {
     
     enum CodingKeys: String, CodingKey {//코딩 컨벤션 통일을 위해 enum으로 정의
         case backDefault = "back_default"
-    }
+    }//JSONDecoder 객체를 생성하고 .keyDecodingStrategy를 .convertFromSnakeCase로 설정하여 더 일관되고 간단하게 키 변환을 하는 방법도 존재함.
 }
